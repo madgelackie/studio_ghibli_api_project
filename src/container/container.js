7e3f634e6822d34e4b727dd77776389a300ff9db
madgelackie <marionmlackie@gmail.com>
2021-10-24 21:16:08 +0100

    frankenstein array

--- src/container/container.js
+++ src/container/container.js
@@ -1,67 +1,60 @@
 import React, {useState, useEffect} from 'react';
-import FilmList from '../components/FlimList';
-import SeenList from '../components/SeenList';
-import WatchList from '../components/WatchList';
-import FilmDetail from '../components/FilmDetail';
+// import FilmList from '../components/FlimList';
+// import SeenList from '../components/SeenList';
+// import WatchList from '../components/WatchList';
+// import FilmDetail from '../components/FilmDetail';
 
 import './container.css'
 
 const Container = () => {
 
     const [films, setFilms] = useState([]);
-    const [people, setPeople] = useState([]);
-    const [selectedFilm, setSelectedFilm] = useState([]);
-    const [watchList, setWatchList] = useState([]);
-    const [seenList, setSeenList] = useState([]);    
-    // const [species, setSpecies] = useState([])
+    // const [selectedFilm, setSelectedFilm] = useState([]);
+    // const [watchList, setWatchList] = useState([]);
+    // const [seenList, setSeenList] = useState([]);    
+
 
     useEffect(() => {
-        fetchFilms();
+        getDetailedFilms();
     }, [])
 
-    useEffect(() => {
-        fetchPeople();
-    }, [])
 
-    const fetchFilms = () => {
+    const getDetailedFilms = () => {
         fetch("https://ghibliapi.herokuapp.com/films")
         .then(response => response.json())
-        .then(films => setFilms(films))
+        .then(films => {
+            const filmPromises = films.map((film) => {
+                return fetch("https://ghibliapi.herokuapp.com/people")
+                .then(response => response.json())
+            })
+        Promise.all(filmPromises).then((allData) => {setFilms(allData)})
+        })
     }
 
-    const fetchPeople = () => {
-        fetch("https://ghibliapi.herokuapp.com/people")
-        .then(response => response.json())
-        .then(people => setPeople(people))
-    }
+    // const onFilmSelected = (film) => {
+    //     setSelectedFilm(film)
+    //     console.log("image clicked")
+    // } 
 
-    const filmsWithPeopleDetail = (films, people) => {
-        filmsWithPeopleDetail
-    }
+    // const addToWatchList = (film) => {
+    //     const updatedWatchList = [...watchList, film]
+    //     setWatchList(updatedWatchList);
+    // }
 
-    const onFilmSelected = (film) => {
-        setSelectedFilm(film)
-        console.log("image clicked")
-    } 
-
-    const addToWatchList = (film) => {
-        const updatedWatchList = [...watchList, film]
-        setWatchList(updatedWatchList);
-    }
-
-    const addToSeenList = (film) => {
-        const updatedSeenList = [...seenList, film]
-        setSeenList(updatedSeenList);
-    }
+    // const addToSeenList = (film) => {
+    //     const updatedSeenList = [...seenList, film]
+    //     setSeenList(updatedSeenList);
+    // }
 
     return (
-        <div id="container-grid">
-            <FilmList films={films} onFilmSelected={onFilmSelected} addToWatchList={addToWatchList} addToSeenList={addToSeenList}/>
-            {watchList ? <WatchList watchList={watchList}/>:null}
-            {seenList ? <SeenList seenList={seenList}/>:null}
-            <FilmDetail selectedFilm={selectedFilm} />
-            {/* {/* <FilmDetail selectedFilm={selectedFilm}/>/} */}
-        </div>
+        <p>did it work?</p>
+        // <div id="container-grid">
+        //     <FilmList films={films} onFilmSelected={onFilmSelected} addToWatchList={addToWatchList} addToSeenList={addToSeenList}/>
+        //     {watchList ? <WatchList watchList={watchList}/>:null}
+        //     {seenList ? <SeenList seenList={seenList}/>:null}
+        //     <FilmDetail selectedFilm={selectedFilm} />
+        //     {/* {/* <FilmDetail selectedFilm={selectedFilm}/>/} */}
+        // </div>
     )
 }
 
